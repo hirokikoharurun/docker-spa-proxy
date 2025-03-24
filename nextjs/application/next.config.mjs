ab78370e1aa972faa75c5bee4b110b2d4e1e1dac
柄本 広樹 <>
2025-03-24 14:29:28 +0900

    add

--- nextjs/application/next.config.mjs
+++ nextjs/application/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  trailingSlash: true,
+  // ↓ SSGの場合に使用する設定
+  // distDir: '../../nginx/html',
+  // output: 'export',
+  // images: {
+  //   unoptimized: true,
+  // }
 };
 
 export default nextConfig;
